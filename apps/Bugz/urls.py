246b69a5950c12571d7b698f0095669ca4f5632d
MichaelKirkaldyV <vmichaelkirkaldy@live.com>
2019-12-15 22:06:49 -0600

    updates to homepage html & css

--- apps/Bugz/urls.py
+++ apps/Bugz/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.homepage)
+    url(r'^', views.homepage),
+    url(r'^', views.contact),
+    url(r'^', views.about),
+    url(r'^', views.login),
 ]
--- apps/Bugz/views.py
+++ apps/Bugz/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render, redirect
 
 def homepage(request):
     return render(request, "Bugz/homepage.html")
+
+def contact(request):
+    return render(request, "Bugz/contact.html")
+
+def about(request):
+    return render(request, "Bugz/about.html")
+
+def login(request):
+    return render(request, "Bugz/login.html")
