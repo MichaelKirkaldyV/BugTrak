0cae1a2a58eb4665f4c3b2b6f3283290195571ea
MichaelKirkaldyV <vmichaelkirkaldy@live.com>
2019-12-29 15:58:09 -0600

    updated errors & sucess messages

--- apps/Bugz/models.py
+++ apps/Bugz/models.py
@@ -1,2 +1,26 @@
 from django.db import models
 
+class QueryManager(models.Manager):
+	def validate_query(request, postData):
+		errors = {}
+
+		if len(postData['name']) < 3:
+			errors['name'] = "Name must be longer than 3 characters"
+
+		if len(postData['email']) < 3:
+			errors['email'] = "Email must be longer than 3 characters"
+
+		if len(postData['company']) < 4:
+			errors['company'] = "Company must be longer than 4 characters"
+
+		if len(postData['message']) < 10:
+			errors['message'] = "Message must be longer than 10 characters"
+
+		return errors
+
+class Query(models.Model):
+	name = models.CharField(max_length=20)
+	email = models.CharField(max_length=30)
+	company = models.CharField(max_length=30)
+	message = models.CharField(max_length=150)
+	objects = QueryManager()
--- apps/Bugz/views.py
+++ apps/Bugz/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.contrib import messages
 
 def homepage(request):
     return render(request, "Bugz/homepage.html")
@@ -14,4 +15,27 @@ def login(request):
     return render(request, "Bugz/login.html")
 
 def process_inquery(request):
-    pass
+    # Sets errors to dictionary in validation query.
+    errors = Query.objects.validate_query(request.POST)
+    print(errors)
+
+    # Checks to see if there are errors.
+    if len(errors):
+        for tag, error in errors.items():
+            messages.error(request, error)
+        return redirect('/contact')
+
+    # Creates Query.
+    else:
+        name = request.POST['name']
+        company = request.POST['company']
+        email = request.POST['email']
+        message = request.POST['message']
+        query = Query.objects.create(name = name, email = email, company = company, message = message)
+        print("Query Created", query)
+        print("Name", name)
+        print("Email", email)
+        print("Company", company)
+        print("Message", message)
+        messages.success(request, "Thank you! We will be in touch.")
+        return redirect('/contact')
