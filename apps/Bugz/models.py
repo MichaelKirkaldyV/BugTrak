256cf1a9f5883ae58fa3d9a0c8c8f34873346314
MichaelKirkaldyV <vmichaelkirkaldy@live.com>
2019-12-31 09:37:59 -0600

    add user class and query class

--- apps/Bugz/models.py
+++ apps/Bugz/models.py
@@ -18,9 +18,33 @@ class QueryManager(models.Manager):
 
 		return errors
 
+class UserManager(models.Manager):
+	def validate_user(request, postData):
+		errors = {}
+
+		if len(postData['email']) < 3:
+			errors['email'] = "Email must be longer than 3 characters"
+
+		if len(postData['username']) < 4:
+			errors['username'] = "Username must be longer than 4 characters"
+
+		if len(postData['password']) < 8:
+			errors['password'] = "Password must be at longer than 7 characters"
+
+		if postData['password'] != postData['cnfrm_password']:
+			errors['cnfrm_password'] = "Passwords do not match"
+		
+		return errors
+
+
 class Query(models.Model):
 	name = models.CharField(max_length=20)
-	email = models.CharField(max_length=30)
 	company = models.CharField(max_length=30)
+	email = models.CharField(max_length=30)
 	message = models.CharField(max_length=150)
 	objects = QueryManager()
+
+class User(models.Model):
+	email = models.CharField(max_length=20)
+	username = models.CharField(max_length=20)
+	password = models.CharField(max_length=20)
