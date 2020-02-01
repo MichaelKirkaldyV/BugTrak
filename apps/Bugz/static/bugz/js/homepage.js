50e74858eafb1ccf6f1560b33bfea2af10a8620e
MichaelKirkaldyV <vmichaelkirkaldy@live.com>
2020-01-31 23:43:52 -0600

    added slideshow to homepage.html

--- apps/Bugz/static/bugz/js/homepage.js
+++ apps/Bugz/static/bugz/js/homepage.js
@@ -1,3 +1,22 @@
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slideshow");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  // Change title every 2 seconds
+  setTimeout(showSlides, 2000); 
+}
+
+//------------------JQUERY------------------------------------//
+
 $(document).ready(function(){
     $("a").hover(function(){
         $(this).addClass("newColor");
